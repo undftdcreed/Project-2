fbfdad1b9ebe5215003b8fd1a39433da228e6702
salmanandoe <saziz2890@gmail.com>
2023-04-11 20:05:43 -0400

    added urls to public

---
+++ public/photos.js
@@ -0,0 +1,63 @@
+module.exports = [
+    {
+        id: "01"
+        name: "I'm one month old today!",
+        img: "https://drive.google.com/file/d/1M89LwyD9bG11bzdCCo_hdzNH64YTEN4E/view?usp=share_link://",
+    }, {
+        id: "02"
+        name: "Me and Mama",
+        img: "https://drive.google.com/file/d/1eziVgwR2jFaMB028bQQPwAj855ACCQ92/view?usp=share_link",
+    }, {
+        id: "03"
+        name: "Meet my sister, Kyda!",
+        img: "https://drive.google.com/file/d/1_bDtHQFL64KmC2qM61fW8hg3YDz2BuGh/view?usp=share_link",
+    }, {
+        id: "04"
+        name: "Getting settled in my new home",
+        img: "https://drive.google.com/file/d/1seXhucYqYBLWSntx-xuV6GUQdxMm8g0I/view?usp=share_link",
+    }, {
+        id: "05"
+        name: "Loving my new bed!",
+        img: "https://drive.google.com/file/d/1mlL6MSApPHCwyk0-fhNXOiSW3Om8bDUp/view?usp=share_link",
+    }, {
+        id: "06"
+        name: "Going for a drive",
+        img: "https://drive.google.com/file/d/15toAagyL4SbDeczusHUt6k5ZW_1jDwW1/view?usp=share_link",
+    }, {
+        id: "07"
+        name: "My favorite dinner, spaghetti and meatballs!",
+        img: "https://drive.google.com/file/d/18JyeMXQ13YgZ7eoJZ5hXKr8HBYYdoBTG/view?usp=share_link",
+    }, {
+        id: "08"
+        name: "Selling my first house",
+        img: "https://drive.google.com/file/d/1u9mH_vJvinThqZ6p6AQCM5YTHm71cOgN/view?usp=share_link",
+    }, {
+        id: "09"
+        name: "Just a rainy car ride to the city",
+        img: "https://drive.google.com/file/d/1fWEvWnvdrO83Hs4lTzAsD0i9txmGTzVs/view?usp=share_link",
+    }, {
+        id: "10"
+        name: "Reunited with my sister!",
+        img: "https://drive.google.com/file/d/1E_3PnpNRVaylBCPjz-nPlfKIjvs1wI0h/view?usp=share_link",
+    }, {
+        id: "11"
+        name: "My favorite place to hang out",
+        img: "https://drive.google.com/file/d/1zGcg7Yh4q55xVFQazlmmytNgYbB5_jio/view?usp=share_link",
+    }, {
+        id: "12"
+        name: "Just a lazy Sunday",
+        img: "https://drive.google.com/file/d/1uvgb_Irpb4VEaPIc84d0jJeXdpD4p7Xq/view?usp=share_link",
+    }, {
+        id: "13"
+        name: "Car naps are the best",
+        img: "https://drive.google.com/file/d/165K1sLpwLjyyUAGVkg6wUYOrmPb8x852/view?usp=share_link",
+    }, {
+        id: "14"
+        name: "Playing hide and seek with mom.. do you think she sees me?",
+        img: "https://drive.google.com/file/d/1rXzlk2SmFSZjDQZFyq7KvfL0QT9_kyYy/view?usp=share_link",
+    }, {
+        id: "15"
+        name: "Road trips are my fave",
+        image: "https://drive.google.com/file/d/1U1Pk2cJOMZupTVHxk_xMP7CiB9x7yxAc/view?usp=share_link",
+    },
+];
